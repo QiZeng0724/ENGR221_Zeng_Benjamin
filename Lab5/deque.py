a848ddbd957fe3dcdadd4e20d4bcc8895a825011
QiZeng0724 <qzeng1@sfsu.edu>
2024-10-14 00:21:15 -0700

    deque.py

--- Lab5/deque.py
+++ Lab5/deque.py
@@ -1,7 +1,7 @@
 """
 Benjamin Zeng
 File Name: deque.py
-Description: 
+Description: Implementing efficient insertion and removal of elements at both ends of the deque
 """
 
 from .doublyLinkedList import DoublyLinkedList
@@ -10,40 +10,54 @@ class Deque():
     def __init__(self):
         self.__values = DoublyLinkedList()
 
+    #Checks if the Deque is empty
     def isEmpty(self):
         return self.__values.isEmpty()
-    
+        #Returns True if the Deque is empty, False otherwise
+
     def __len__(self):
         return len(self.__values)
+        #Returns the number of elements in the Deque
     
     def __str__(self):
         return str(self.__values)
-
+        #Returns a string representation of the Deque
+    
     def peekLeft(self):
         if self.isEmpty():
             raise Exception("Deque is empty")
+            #Raises Exception if the Deque is empty
         return self.__values.getFirstNode().getValue()
-
+        #Returns the value of the leftmost element in the Deque
+    
     def peekRight(self):
         if self.isEmpty():
             raise Exception("Deque is empty")
+            #Raises Exception if the Deque is empty
         return self.__values.getLastNode().getValue()
+        #Returns the value of the rightmost element in the Deque
 
     def insertLeft(self, value):
         self.__values.insertFront(value)
+        #Inserts a value at the left end of the Deque
         
     def insertRight(self, value): 
         self.__values.insertBack(value)
+        #Inserts a value at the right end of the Deque
 
     def removeLeft(self): 
         if self.isEmpty():
             raise Exception("Deque is empty")
+            #Raises Exception if the Deque is empty
         return self.__values.deleteFirstNode()
+        #Removes and returns the value of the leftmost element in the Deque
 
     def removeRight(self):
         if self.isEmpty():
             raise Exception("Deque is empty")
+            #Raises Exception if the Deque is empty
         return self.__values.deleteLastNode()
+        #Removes and returns the value of the rightmost element in the Deque
     
 if __name__ == "__main__":
     pass
